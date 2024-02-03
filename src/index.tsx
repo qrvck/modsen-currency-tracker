a42a3770033394b44fdf2f691f9e56185b0c6942
Vlad Kapitan <v.kapitan2010@gmail.com>
2024-02-04 01:31:40 +0300

    feat: add global styles

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
+import { GlobalStyles } from './styles';
 import { ThemeProvider } from './themes/ThemeProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
@@ -11,6 +12,7 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <ThemeProvider>
+        <GlobalStyles />
         <App />
       </ThemeProvider>
     </BrowserRouter>
---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,71 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+  html {
+    scroll-behavior: smooth;
+    color-scheme: ${({ theme }) => (theme.isDark ? 'dark' : 'light')};
+  }
+
+  body {
+    margin: 0;
+    font-family: ${({ theme }) => theme.fonts.primary};
+    font-size: 14px;
+    color: ${({ theme }) => theme.colors.inverted.first};
+    background-color: ${({ theme }) => theme.colors.inverted.second};
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    overflow-y: scroll;
+  }
+
+  #root {
+    display: grid;
+    grid-template-rows: auto 1fr auto;
+    min-height: 100vh;
+  }
+
+  img {
+    max-width: 100%;
+    height: auto;
+    vertical-align: bottom;
+  }
+
+  ul,
+  ol {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+  }
+
+  *,
+  *::before,
+  *::after {
+    margin: 0;
+    padding: 0;
+    outline: none;
+    box-sizing: border-box;
+    transition: color, background-color 0.3s ease;
+  }
+
+  button,
+  input {
+    font: inherit;
+    color: inherit;
+    border: none;
+    background-color: transparent;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  svg {
+    vertical-align: bottom;
+  }
+
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+`;
+
+export { GlobalStyles };
