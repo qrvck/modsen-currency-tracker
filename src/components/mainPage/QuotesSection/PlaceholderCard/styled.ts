5c72e22a7c4400038c1990285a134ca6dd511089
Vlad Kapitan <v.kapitan2010@gmail.com>
2024-02-13 00:03:31 +0300

    refactor: change view of placeholder card

--- src/components/mainPage/QuotesSection/PlaceholderCard/styled.ts
+++ src/components/mainPage/QuotesSection/PlaceholderCard/styled.ts
@@ -4,8 +4,13 @@ import { Card as QuoteCard } from '../QuoteCard/styled';
 import { skeletonAnimation } from './styled.keyframes';
 
 const Card = styled(QuoteCard)<{ $isAnimated: boolean }>`
+  cursor: auto;
   opacity: 0.85;
   animation: 2s ease-in-out 0.5s infinite ${({ $isAnimated }) => $isAnimated && skeletonAnimation};
+
+  &:hover {
+    opacity: 0.85;
+  }
 `;
 
 export { Card };
