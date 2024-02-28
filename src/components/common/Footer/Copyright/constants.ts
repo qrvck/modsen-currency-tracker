4ad816ab889497db965cbef3d5617cfd8a1136c1
Vlad Kapitan <v.kapitan2010@gmail.com>
2024-02-29 00:15:05 +0300

    refactor: refactor Copyright component

--- src/components/common/Footer/Copyright/constants.ts
+++ src/components/common/Footer/Copyright/constants.ts
@@ -1,2 +1,3 @@
 export const COLPYRIGHT_TEXT_1 = 'Startsup © 2023-';
 export const COLPYRIGHT_TEXT_2 = ', All Rights Reserved';
+export const YEAR = new Date().getFullYear();
--- src/components/common/Footer/Copyright/index.tsx
+++ src/components/common/Footer/Copyright/index.tsx
@@ -1,12 +1,10 @@
-import React, { useMemo } from 'react';
+import React from 'react';
 
-import { COLPYRIGHT_TEXT_1, COLPYRIGHT_TEXT_2 } from './constants';
+import { COLPYRIGHT_TEXT_1, COLPYRIGHT_TEXT_2, YEAR } from './constants';
 import { Text } from './styled';
 
 function Copyright() {
-  const year = useMemo(() => new Date().getFullYear(), []);
-
-  return <Text>{`${COLPYRIGHT_TEXT_1}${year}${COLPYRIGHT_TEXT_2}`}</Text>;
+  return <Text>{`${COLPYRIGHT_TEXT_1}${YEAR}${COLPYRIGHT_TEXT_2}`}</Text>;
 }
 
 export { Copyright };
